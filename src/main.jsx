dc3c35a1ce839af270bb6ebf8dc3c93be4dfa8a1
Saru-Jayaraman <sarumathijayaraman@gmail.com>
2024-11-14 20:23:12 +0530

    Marketplace Application implementation using React.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-<b>Hello JS</b>
-)
+createRoot(document.getElementById('root')).render(<App></App>);
